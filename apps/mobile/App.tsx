086b0e365e47f19501dede08b8dc6edd5770fe82
Mubin Ansari <mubinansari910@gmail.com>
2023-01-20 02:48:48 +0530

    use light status bar in mobile

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -6,6 +6,7 @@ import { api } from "$trpc";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
 import superjson from "superjson";
 import colors from "tailwindcss/colors";
 import Home from "./screens/Home";
@@ -33,6 +34,7 @@ const App = () => {
           className="h-[100vh]"
         >
           <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar style="light" />
             <Home />
           </SafeAreaView>
         </LinearGradient>
