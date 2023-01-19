a854a6bfa6a2dff1f4017292037cefb152c8aea1
Mubin Ansari <mubinansari910@gmail.com>
2023-01-19 23:23:01 +0530

    remove redundant tauri + nextjs setup

--- apps/desktop/src/utils/trpc.ts
+++ apps/desktop/src/utils/trpc.ts
@@ -1,52 +1,5 @@
-import { httpBatchLink, loggerLink } from "@trpc/client";
-import { createTRPCNext } from "@trpc/next";
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
-import superjson from "superjson";
+import { createTRPCReact } from "@trpc/react-query";
 
-import { type AppRouter } from "$server";
+import type { AppRouter } from "$server";
 
-const getBaseUrl = () => {
-  return `http://localhost:3000`;
-};
-
-/**
- * A set of typesafe react-query hooks for your tRPC API
- */
-export const api = createTRPCNext<AppRouter>({
-  config() {
-    return {
-      /**
-       * Transformer used for data de-serialization from the server
-       * @see https://trpc.io/docs/data-transformers
-       **/
-      transformer: superjson,
-
-      /**
-       * Links used to determine request flow from client to server
-       * @see https://trpc.io/docs/links
-       * */
-      links: [
-        loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error),
-        }),
-        httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
-      ],
-    };
-  },
-  ssr: false,
-});
-
-/**
- * Inference helper for inputs
- * @example type HelloInput = RouterInputs['example']['hello']
- **/
-export type RouterInputs = inferRouterInputs<AppRouter>;
-/**
- * Inference helper for outputs
- * @example type HelloOutput = RouterOutputs['example']['hello']
- **/
-export type RouterOutputs = inferRouterOutputs<AppRouter>;
+export const api = createTRPCReact<AppRouter>();
--- apps/desktop/tailwind.config.js
+++ apps/desktop/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
